599ec8288a2c7695435438228ad2f73f880a65ed
dieh <madycolavitti08@gmail.com>
2026-04-28 00:45:09 +0200

    Update vCard and thumbnail image in handler.js

--- handler.js
+++ handler.js
@@ -641,7 +641,7 @@ export async function callUpdate(callUpdate) {
   for (let nk of callUpdate) {
     if (!nk.isGroup && nk.status === 'offer') {
       let callmsg = await this.reply(nk.from, `ciao @${nk.from.split('@')[0]}, c'è anticall.`, false, { mentions: [nk.from] }).catch(console.error)
-      let vcard = `BEGIN:VCARD\nVERSION:5.0\nN:;𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲;;;\nFN:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nORG:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nTITLE:\nitem1.TEL;waid=393773842461:+39 3515533859\nitem1.X-ABLabel:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nEND:VCARD`
+      let vcard = `BEGIN:VCARD\nVERSION:5.0\nN:;𝑺𝑳𝒀𝑪𝑬 𝑩𝑶𝑻;;;\nFN:𝑺𝑳𝒀𝑪𝑬 𝑩𝑶𝑻\nORG:𝑺𝑳𝒀𝑪𝑬 𝑩𝑶𝑻nTITLE:\nitem1.TEL;waid=393773842461:+39 3515533859\nitem1.X-ABLabel:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:𝑺𝑳𝒀𝑪𝑬 𝑩𝑶𝑻\nEND:VCARD`
       await this.sendMessage(nk.from, { contacts: { displayName: 'Unlimited', contacts: [{ vcard }] } }, { quoted: callmsg }).catch(console.error)
       await this.updateBlockStatus(nk.from, 'block').catch(console.error)
     }
@@ -677,7 +677,7 @@ global.dfail = (type, m, conn) => {
         title: `${msg}`,
         body: '',
         previewType: 'PHOTO',
-        thumbnail: fs.readFileSync('./media/principale.jpeg'),
+        thumbnail: fs.readFileSync('./media/menu.jpeg'),
         mediaType: 1,
         renderLargerThumbnail: true
       }
