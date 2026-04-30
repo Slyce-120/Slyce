989cd381e5f2342773a0e7874f201520d650f71e
slyce-120 <psjq55h4fh@privaterelay.appleid.com>
2026-05-01 01:14:48 +0200

    Update owner-riavvio.js

--- plugins/owner/owner-riavvio.js
+++ plugins/owner/owner-riavvio.js
@@ -4,7 +4,7 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const handler = async (m, { conn }) => {
     const userId = m.sender;
     const groupId = m.chat;
-    const nomeDelBot = conn.user?.name || global.db?.data?.nomedelbot || 'ChatUnity';
+    const nomeDelBot = conn.user?.name || global.db?.data?.nomedelbot || '𝑺𝑳𝒀𝑪𝑬 𝑩𝑶𝑻';
     
     try {
         const { key } = await conn.sendMessage(m.chat, {
