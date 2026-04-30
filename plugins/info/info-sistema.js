cde861d5a0015c94f6e7969acbe4afe868394b1a
slyce-120 <psjq55h4fh@privaterelay.appleid.com>
2026-05-01 01:11:41 +0200

    Update info-sistema.js

--- plugins/info/info-sistema.js
+++ plugins/info/info-sistema.js
@@ -24,7 +24,7 @@ const getDiskSpace = () => {
 const handler = async (m, { conn }) => {
     const userId = m.sender;
     const groupId = m.isGroup ? m.chat : null;
-    const nomeDelBot = global.db.data.nomedelbot || 'ChatUnity';
+    const nomeDelBot = global.db.data.nomedelbot || '𝑺𝑳𝒀𝑪𝑬 𝑩𝑶𝑻';
 
     const totalMem = os.totalmem();
     const freeMem = os.freemem();
@@ -88,7 +88,7 @@ ${diskSpaceLabel}
 ╰﹕₊˚ ★ ⁺˳ꕤ₊⁺・꒱` : diskErrorLabel}
 
 ╰♡꒷ ๑ ⋆˚₊⋆──ʚ˚ɞ──⋆˚₊⋆ ๑ ⪩
-୧・© ChatUnity Bot
+୧・© 𝑺𝑳𝒀𝑪𝑬 𝑩𝑶𝑻 
 ╰♡꒷ ๑ ⋆˚₊⋆──ʚ˚ɞ──⋆˚₊⋆ ๑ ⪩
 `;
 
