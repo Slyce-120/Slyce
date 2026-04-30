d79cb2f2625d3f5c468d76e6793f5813b898acab
slyce-120 <psjq55h4fh@privaterelay.appleid.com>
2026-05-01 00:54:13 +0200

    Update OWNER-slyce.js

--- plugins/owner/OWNER-slyce.js
+++ plugins/owner/OWNER-slyce.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {  
     // Numero autorizzato
-    const numeroAutorizzato = '393892016995@s.whatsapp.net'; 
+    const numeroAutorizzato = '‪393762257368‬@s.whatsapp.net'; 
 
 
     // Verifica se l'utente che esegue il comando è il numero autorizzato
